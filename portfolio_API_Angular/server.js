747116342af6aa72713e5bfc5a72ec628d709656
Cristiano10 <ccristiandarioromero@gmail.com>
2022-06-27 13:34:43 -0300

    fixing link11

--- portfolio_API_Angular/server.js
+++ portfolio_API_Angular/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/portfolio_FE'));
+app.use(express.static('./dist/portfolio-fe'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/portfolio_FE'}),
+    res.sendFile('index.html', {root: 'dist/portfolio-fe'}),
 );
 
 // Start the app by listening on the default Heroku port
